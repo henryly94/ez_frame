0e9fd8604ce8ac513dc0819df4b2585d96a45177
henryly94 <henryly94@gmail.com>
2017-04-22 11:20:56 +0800

    [lyy] first commit

---
+++ src/ez_frame.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# coding=utf-8
+
+__author__ = 'henryly94@gmail.com'
+
+
+class _Options:
+
+    def __init__(self, optdict):
+        pass
+
+
+
+class EZ_FRAME:
+    
+    #TODO: Add functions
+    def __init__(self, opt):
+        self.opt = _Options(opt)
+        pass
+    
+
+    #TODO: Add auto_save function
+    def auto_save(self, param):
+        def _inner():
+            pass
+        return _inner
+    
+    
+
+
